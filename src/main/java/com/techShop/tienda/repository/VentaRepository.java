69902e082fdb7e9ce00886eb6e2edac688ba3da9
Brandon Solis <160693594+brandonso80035@users.noreply.github.com>
2026-04-05 15:36:35 -0600

    Removing Last Commit
    Error uploading files to Github.

--- src/main/java/com/techShop/tienda/repository/VentaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.techShop.tienda.repository;
-
-import com.techShop.tienda.domain.Venta;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VentaRepository extends JpaRepository<Venta, Integer>{
-    
-}
